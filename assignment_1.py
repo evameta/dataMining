da2e349799ed42d9cd4118dbd08ca52341193601
eva <43240169+evameta@users.noreply.github.com>
2019-04-16 13:02:11 +0200

    Added docstrings

--- assignment_1.py
+++ assignment_1.py
@@ -9,7 +9,11 @@ LOOK_BACK = 8
 
 
 def load_data():
+    """
+    Load raw data from the file into a pandas dataframe
 
+    :return: dataframe containing raw data
+    """
     headers = ['row', 'id', 'time', 'variable', 'value']
     data_types = [int, str, str, str, float]
 
@@ -23,7 +27,11 @@ def load_data():
 
 
 def mood_dataframe():
+    """
+    Initialised empty dataframe with the necessary column names, depending on how far we are planning to look back
 
+    :return: empty dataframe with correct column names
+    """
     raw_columns = ['id', 'date']
 
     for day in range(LOOK_BACK):
@@ -33,7 +41,13 @@ def mood_dataframe():
 
 
 def prepare_mood_data(raw_data):
+    """
+    Prepares the raw data for mood processing in a way that facilitates further actions. We move the data from rows
+    into separate columns.
 
+    :param raw_data:    dataframe containing the raw data for mood processing
+    :return:            dataframe with pivoted data
+    """
     try:
         with open('mood.csv') as file_:
             return pd.read_csv(file_)
@@ -63,13 +77,16 @@ def prepare_mood_data(raw_data):
     return mood_final
 
 
-def enrich_mood_data():
-
-    return
-
-
 def describe_period(data, period):
+    """
+    Add columns to the dataframe describing the min, max, and mean behaviour of a relative period of time,
+    i.e. if period = 5, we look at all the days between today and today - 5 days.
 
+    :param data:      dataframe containing the raw data for the relevant period
+    :param period:    integer representing the amount of days we look back on
+    :return:          input dataframe with additional columns describing mean, min, and max behaviour for the period
+                      of time in question.
+    """
     mean_cols = ['mean_{}'.format(n) for n in range(period)]
     min_cols = ['min_{}'.format(n) for n in range(period)]
     max_cols = ['max_{}'.format(n) for n in range(period)]
@@ -84,7 +101,9 @@ def describe_period(data, period):
 
 
 def main():
-
+    """
+    Main Function
+    """
     data = load_data()
     mood = prepare_mood_data(data.loc[data['variable'] == 'mood'])
 
