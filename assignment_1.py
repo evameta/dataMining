3637b925211adfd95928099068184bb8015f8e23
eva <43240169+evameta@users.noreply.github.com>
2019-04-18 14:51:35 +0200

    Updated version of Assignment 1 with more complete pre-processing

--- assignment_1.py
+++ assignment_1.py
@@ -21,11 +21,16 @@ def load_data():
     headers = ['row', 'id', 'time', 'variable', 'value']
     data_types = [int, str, str, str, float]
 
-    with open('data.csv') as file_:
-        data = pd.read_csv(file_, skiprows=1, names=headers, parse_dates=['time'], dtype=dict(zip(headers, data_types)))
+    params = {'skiprows': 1,
+              'names': headers,
+              'parse_dates': ['time'],
+              'dtype': dict(zip(headers, data_types))}
 
-    data.set_index(keys=['row'], drop=True, inplace=True)
+    with open('data.csv') as file_:
+        data = pd.read_csv(file_, **params)
+
     data['date'] = data['time'].map(lambda x: x.date())
+    data = data.set_index(keys=['id', 'date'], drop=True)
 
     return data
 
@@ -54,7 +59,7 @@ def get_values(data, key_id, key_date, delta):
         return None, False
 
 
-def prepare_mood_data(raw_data):
+def enrich_mood(raw_data):
     """
     Prepares the raw data for mood processing in a way that facilitates further actions. We move the data from rows
     into separate columns.
@@ -121,22 +126,64 @@ def describe_period(data, period):
     return data
 
 
+def add_variable(raw_data, enriched, column_name, method='sum'):
+    """
+
+    :param raw_data:
+    :param enriched:
+    :param column_name:
+    :param method:
+    :return:
+    """
+    cols = ['id', 'date', column_name]
+    out_data = pd.DataFrame(columns=cols)
+
+    for key, values in raw_data.groupby(['id', 'date']):
+
+        if method == 'sum':
+            value = values['value'].sum()
+        elif method == 'mean':
+            value = values['value'].mean()
+        elif method == 'max':
+            value = values['value'].max()
+        else:
+            raise RuntimeError('Incorrect method in adding variable')
+
+        row = [*key, value]
+        out_data = out_data.append(dict(zip(cols, row)), ignore_index=True)
+
+    out_data.set_index(['id', 'date'], inplace=True)
+    enriched = pd.concat([enriched, out_data], axis=1)
+
+    return enriched
+
+
 def enrich_data(data):
     """
 
     :param data:
     :return:
     """
-    mood = prepare_mood_data(data.loc[data['variable'] == 'mood'])
-
+    mood = enrich_mood(data.loc[data['variable'] == 'mood'])
+    weekdays = pd.get_dummies(mood['date'].apply(lambda x: x.weekday()), prefix='weekday')
     for period in [1, 2, 3, 5, 7]:
         mood = describe_period(mood, period)
+    enriched = pd.concat([mood, weekdays], axis=1)
 
-    mood = pd.concat([pd.get_dummies(mood['date'].apply(lambda x: x.weekday()), prefix='weekday'), mood], axis=1)
-    mood.to_csv('mood.csv', index=False)
+    enriched.set_index(['id', 'date'], inplace=True)
+
+    enriched = add_variable(data.loc[data['variable'] == 'screen'], enriched, 'screen_time')
+    #enriched = add_variable(data.loc[data['variable'] == 'call'], enriched, 'call', method='max')
+    #enriched = add_variable(data.loc[data['variable'] == 'activity'], enriched, 'screen_time', method='mean')
+    #enriched = add_variable(data.loc[data['variable'] == 'circumplex.valence'], enriched, 'valence', method='mean')
+    #enriched = add_variable(data.loc[data['variable'] == 'circumplex.arousal'], enriched, 'arousal', method='mean')
+
+    enriched = enriched.dropna(subset=['output'])
+    enriched = enriched.fillna(0)
+
+    enriched.to_csv('mood.csv')
 
     X = mood.drop(['output', 'id', 'date'], axis=1)
-    X = X.fillna(0)
     y = mood['output'].astype(int)
 
     return X, y
@@ -144,6 +191,7 @@ def enrich_data(data):
 
 def preprocess(method='plain'):
     """
+
 
     :param method:
     :return:
