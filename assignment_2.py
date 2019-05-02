037ecd03b81752197e9848366226ee13eb468f93
Kaspernicholas <kasperanicholas@gmail.com>
2019-05-02 16:55:14 +0200

    added some stuff yo

--- assignment_2.py
+++ assignment_2.py
@@ -3,6 +3,9 @@ Hello
 """
 import csv
 import random
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 TRAIN = 'training_set_VU_DM.csv'
 LEN_TRAIN = 4958347
@@ -26,5 +29,18 @@ def random_sample(k=150):
                 writer.writerow(line)
 
 
-random_sample()
+def corr_matrix():
+    df = pd.read_csv("data/training_set_VU_DM.csv")
+    #df = pd.read_csv("data/training_sample.csv")
+    mpl.rcParams.update({'font.size': 5})
+    columns = list(df)
+    ax = plt.imshow(df.corr(), cmap='hot', interpolation='nearest')
+    plt.xticks(ticks=[i for i in range(len(columns))], labels=columns, rotation='vertical')
+    plt.yticks(ticks=[i for i in range(len(columns))], labels=columns)
+    plt.colorbar().set_label('correlation',fontsize=10)
+    plt.savefig('corr_matrix.pdf',  bbox_inches="tight")
+    plt.show()
 
+
+if __name__ == '__main__':
+    corr_matrix()
