ce73c6f1e8230271f0afd6b1bea72a077959093a
eva <43240169+evameta@users.noreply.github.com>
2019-05-02 16:56:16 +0200

    Added count null values code

--- assignment_2.py
+++ assignment_2.py
@@ -4,6 +4,8 @@ Hello
 import csv
 import random
 
+from operator import add
+
 TRAIN = 'training_set_VU_DM.csv'
 LEN_TRAIN = 4958347
 
@@ -26,5 +28,33 @@ def random_sample(k=150):
                 writer.writerow(line)
 
 
-random_sample()
+def run(reload=False):
+
+    if reload:
+        random_sample()
+
+
+def count_missing_values():
+
+    with open('data/' + TRAIN) as file_:
+        reader = csv.reader(file_)
+
+        headers = next(reader)
+        none_values = [0 for _ in headers]
+
+        for i, line in enumerate(reader):
+            none_values = list(map(add, none_values, [elem == 'NULL' for elem in line]))
+
+            if i % 10000 == 0:
+                print(i, 'iterations complete!')
+
+    with open('data/none_values.csv', 'w', newline='') as out_:
+        writer = csv.writer(out_)
+        writer.writerow(headers)
+        writer.writerow(none_values)
+
+    return
+
+if __name__ == '__main__':
+    count_missing_values()
 
