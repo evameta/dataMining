31f4ee990c63622ebe847596553a3ee1316bcd8a
Kaspernicholas <kasperanicholas@gmail.com>
2019-05-22 11:13:12 +0200

    added plots and partial cleaning of data

--- assignment_2.py
+++ assignment_2.py
@@ -6,10 +6,12 @@ import random
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 from operator import add
 
 TRAIN = 'training_set_VU_DM.csv'
+SAMPLE = 'training_sample.csv'
 LEN_TRAIN = 4958347
 
 
@@ -65,12 +67,77 @@ def corr_matrix():
     mpl.rcParams.update({'font.size': 5})
     columns = list(df)
     ax = plt.imshow(df.corr(), cmap='hot', interpolation='nearest')
-    plt.xticks(ticks=[i for i in range(len(columns))], labels=columns, rotation='vertical')
-    plt.yticks(ticks=[i for i in range(len(columns))], labels=columns)
+    plt.xticks(ticks=[i-1 for i in range(len(columns))], labels=columns, rotation='vertical')
+    plt.yticks(ticks=[i-1 for i in range(len(columns))], labels=columns)
     plt.colorbar().set_label('correlation',fontsize=10)
     plt.savefig('corr_matrix.pdf',  bbox_inches="tight")
     plt.show()
 
 
+
+def statistics_data():
+    df = pd.read_csv("data/" + TRAIN)
+    df.drop(columns=['date_time'])
+    df_none = pd.read_csv("data/none_values.csv").drop(columns=['date_time']).transpose()
+    std_dev = df._get_numeric_data().std(axis=0)
+    print(std_dev)
+    mean = df._get_numeric_data().mean(axis=0)
+    print(mean)
+    stats = {'mean': mean, 'std_dev': std_dev} # 'missing_values': df_none}
+    df_stats = pd.DataFrame(stats)
+    df_stats = pd.concat([df_stats, df_none], axis=1, sort=False)
+    df_stats.rename(columns={0: 'missing_values'}, inplace=True)
+    df_stats['missing_values'] = df_stats['missing_values'].div(LEN_TRAIN)
+    df_stats.to_csv('data/stats.csv')
+    print(df_stats)
+
+
+def plot_data():
+    df = pd.read_csv("data/" + TRAIN)
+    pos_clicked = df['position'].where(df['click_bool'] == 1)
+    pos_booked = df['position'].where(df['booking_bool'] == 1)
+    pos_clicked = pos_clicked.value_counts(normalize=True)
+    pos_booked = pos_booked.value_counts(normalize=True)
+    fig, ax = plt.subplots(figsize=[15, 5])
+    x = np.arange(1, 41)
+    ax.bar(x - 0.2, pos_clicked[x], width=0.4, label='Clicked', color='red')
+    ax.bar(x + 0.2, pos_booked[x], width=0.4, label='Booked', color='green')
+    ax.set_xticks(x)
+    ax.set_yticklabels(['{:.1%}'.format(i) for i in np.linspace(0, 0.2, 9)])
+    plt.legend(fancybox=True, loc=1)
+    plt.savefig('click_booked_pos.pdf', bbox_inches='tight')
+    plt.show()
+
+    # plot hist of positions of booked hotels where ranking was random vs. ordered
+    pos_random = df['position'].where(df['booking_bool'] == 1).where(df['random_bool'] == 1)
+    pos_ordered = df['position'].where(df['booking_bool'] == 1).where(df['random_bool'] == 0)
+    pos_random = pos_random.value_counts(normalize=True)
+    pos_random.index = pos_random.index.map(int)
+    pos_ordered = pos_ordered.value_counts(normalize=True)
+    pos_ordered.index = pos_ordered.index.map(int)
+
+    fig, ax = plt.subplots(figsize=[15, 5])
+    x = np.arange(1, 41)
+    ax.bar(x - 0.2, pos_random[x], width=0.4, label='Random', color='red')
+    ax.bar(x + 0.2, pos_ordered[x], width=0.4, label='Ordered', color='green')
+    ax.set_xticks(x)
+    ax.set_yticklabels(['{:.1%}'.format(i) for i in np.linspace(0, 0.2, 9)])
+    plt.legend(fancybox=True, loc=1)
+    plt.savefig('random_booked_pos.pdf', bbox_inches='tight')
+    plt.show()
+
+def clean_data():
+
+    df = pd.read_csv("data/" + SAMPLE)
+
+    # Set missing competitor data to 0.
+    filter_col = [col for col in df if col.startswith('comp')]
+    df[filter_col] = df[filter_col].fillna(0)
+
+    # Remove column if not useful or missing data.
+    df = df.drop(columns=['date_time', 'srch query affinity score'])
+    df.groupby()
+
+
 if __name__ == '__main__':
-    corr_matrix()
+    clean_data()
